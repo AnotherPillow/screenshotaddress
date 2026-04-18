0deadc18cfd31fdadafe32e5de84ea06745972a5
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2026-04-18 17:47:51 +0100

    Initial commit

---
+++ src/main/java/rocks/pillow/screenshotaddress/Screenshotaddress.java
@@ -0,0 +1,10 @@
+package rocks.pillow.screenshotaddress;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Screenshotaddress implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/rocks/pillow/screenshotaddress/client/ScreenshotaddressClient.java
@@ -0,0 +1,12 @@
+package rocks.pillow.screenshotaddress.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
+
+public class ScreenshotaddressClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

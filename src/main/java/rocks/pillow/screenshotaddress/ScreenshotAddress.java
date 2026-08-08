1119fe4a37070f00785a805e4a7feb186add7f8c
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2026-08-08 02:48:20 +0100

    redo server & change main

--- src/main/java/rocks/pillow/screenshotaddress/ScreenshotAddress.java
+++ src/main/java/rocks/pillow/screenshotaddress/ScreenshotAddress.java
@@ -1,7 +1,7 @@
 package rocks.pillow.screenshotaddress;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +9,9 @@ public class ScreenshotAddress implements ModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("template");
-    public static final String VERSION = /*$ mod_version*/ "0.1.0";
-    public static final String MINECRAFT = /*$ minecraft*/ "26.1.2";
+    public static final Logger LOGGER = LoggerFactory.getLogger("ScreenshotAddress");
+//    public static final String VERSION = /*$ mod_version*/ "1.1.0";
+//    public static final String MINECRAFT = /*$ minecraft*/ "26.2";
 
     @Override
     public void onInitialize() {
@@ -19,20 +19,16 @@ public class ScreenshotAddress implements ModInitializer {
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
 
-        //? if !release
-        LOGGER.info("Hello Fabric world!");
 
-        LOGGER.warn("I'm still a template!");
-        LOGGER.warn("I'm still a template!");
     }
 
     /**
-     * Adapts to the {@link Identifier} changes introduced in 1.21.
+     * Adapts to the {@link ResourceLocation} changes introduced in 1.21.
      */
-    public static Identifier id(String namespace, String path) {
+    public static ResourceLocation id(String namespace, String path) {
         //? if <1.21 {
-        /*return new Identifier(namespace, path);
+        /*return new ResourceLocation(namespace, path);
         *///?} else
-        return Identifier.fromNamespaceAndPath(namespace, path);
+        return ResourceLocation.fromNamespaceAndPath(namespace, path);
     }
 }
---
+++ src/main/java/rocks/pillow/screenshotaddress/Server.java
@@ -0,0 +1,28 @@
+package rocks.pillow.screenshotaddress;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ServerData;
+
+public class Server {
+    private static String getCurrentRawAddress() {
+        Minecraft client = Minecraft.getInstance();
+        ServerData server = client.getCurrentServer();
+        return server == null ? null : server.ip;
+    }
+
+    public static String getCurrentLocation() {
+        Minecraft client = Minecraft.getInstance();
+
+        if (client.level == null) return "menus";
+        if (client.isSingleplayer()) return "singleplayer";
+        return getCurrentRawAddress();
+    }
+
+    public static String getCurrentLocationSafe() {
+        try {
+            return getCurrentLocation().replace(":", "_");
+        } catch (Exception e) {
+            return "unknown";
+        }
+    }
+}

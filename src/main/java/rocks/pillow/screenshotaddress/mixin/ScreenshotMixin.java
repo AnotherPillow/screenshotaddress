79b557757ddb936aa0ff7312d5ea423044c87932
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2026-08-08 02:49:26 +0100

    redo mixin

--- src/main/java/rocks/pillow/screenshotaddress/mixin/ScreenshotMixin.java
+++ src/main/java/rocks/pillow/screenshotaddress/mixin/ScreenshotMixin.java
@@ -1,15 +1,32 @@
 package rocks.pillow.screenshotaddress.mixin;
 
+import net.minecraft.client.Screenshot;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import rocks.pillow.screenshotaddress.Server;
 
-@Mixin(MinecraftServer.class)
+import java.io.File;
+
+@Mixin(Screenshot.class)
 public class ScreenshotMixin {
-	@Inject(method = "loadLevel", at = @At("HEAD"))
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	@Inject(
+			method = "getFile",
+			at = @At("RETURN"),
+			cancellable = true
+	)
+	private static void getScreenshotFilename(
+			File directory,
+			CallbackInfoReturnable<File> cir
+	) {
+		File originalFile = cir.getReturnValue();
+		String filename = originalFile.getName();
+
+		cir.setReturnValue(new File(directory,
+				filename.replace(".png", "") + "_" + Server.getCurrentLocationSafe() + ".png"
+		));
 	}
 }
